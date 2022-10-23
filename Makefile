206dc50f5119268756c3f326588e17c855e87082
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-23 13:09:32 -0700

    restructure

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ default:
 	cp -r /root/lib/x86_64-linux-gnu/* /usr/local/lib
 	cp -r /root/lib/* /usr/local/lib
 	
-	g++ -std=c++17 -O3 src/server.cc -larrow -larrow_dataset -ldemikernel -o main
+	g++ -std=c++17 -O3 src/main.cc -larrow -larrow_dataset -ldemikernel -o main
--- client.sh
+++ client.sh
@@ -5,6 +5,6 @@ export CONFIG_PATH=/users/noobjc/demikernel/client.yaml
 export MTU=1500
 export MSS=1500
 export LIBOS=catnip
-export LD_LIBRARY_PATH=/root/lib/x86_64-linux-gnu
+export LD_LIBRARY_PATH=/root/lib:/root/lib/x86_64-linux-gnu
 
 ./main --client 10.10.1.2 3000
