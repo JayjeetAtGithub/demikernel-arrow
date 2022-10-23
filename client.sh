7f82269c50ee4e1f608102a6bc9056cc9c298f8e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-23 12:28:43 -0700

    update scripts to set the LD_LIBRARY_PATH environment variable

--- client.sh
+++ client.sh
@@ -5,5 +5,6 @@ export CONFIG_PATH=/users/noobjc/demikernel/client.yaml
 export MTU=1500
 export MSS=1500
 export LIBOS=catnip
+export LD_LIBRARY_PATH=/root/lib/x86_64-linux-gnu
 
 ./code --client 10.10.1.2 3000
--- server.sh
+++ server.sh
@@ -5,5 +5,6 @@ export CONFIG_PATH=/users/noobjc/demikernel/server.yaml
 export MTU=1500
 export MSS=1500
 export LIBOS=catnip
+export LD_LIBRARY_PATH=/root/lib/x86_64-linux-gnu
 
 ./server --server 10.10.1.2 3000
