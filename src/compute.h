dc24ae8f011e7f2810531e364e043ea1b2d3f3b5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-11-18 12:11:46 -0800

    commit changes

--- src/compute.h
+++ src/compute.h
@@ -24,13 +24,32 @@
 
 namespace cp = arrow::compute;
 
-std::shared_ptr<arrow::RecordBatch> UnpackRecordBatch(uint8_t *buf, size_t len) {
-    auto buffer = std::make_shared<arrow::Buffer>(buf, len);
-    auto buffer_reader = std::make_shared<arrow::io::BufferReader>(buffer);
-    auto reader = arrow::ipc::RecordBatchFileReader::Open(buffer_reader,
-        arrow::ipc::IpcReadOptions::Defaults()).ValueOrDie();
-    auto batch = reader->ReadRecordBatch(0).ValueOrDie();
-    return batch;
+std::shared_ptr<arrow::Buffer> PackRecordBatch(const std::shared_ptr<arrow::RecordBatch>& batch) {
+  ARROW_ASSIGN_OR_RAISE(auto buffer_output_stream,
+                        arrow::io::BufferOutputStream::Create());
+
+  auto options = arrow::ipc::IpcWriteOptions::Defaults();
+  auto codec = arrow::Compression::LZ4_FRAME;
+
+  ARROW_ASSIGN_OR_RAISE(options.codec, arrow::util::Codec::Create(codec));
+  ARROW_ASSIGN_OR_RAISE(auto writer, arrow::ipc::MakeStreamWriter(
+                                        buffer_output_stream, batch->schema(), options));
+
+  ARROW_RETURN_NOT_OK(writer->WriteRecordBatch(*batch));
+  ARROW_RETURN_NOT_OK(writer->Close());
+
+  ARROW_ASSIGN_OR_RAISE(auto buffer, buffer_output_stream->Finish());
+  return buffer;
+}
+
+std::shared_ptr<arrow::RecordBatch> UnpackRecordBatch(uint8_t* data, int32_t size) {
+  auto buffer = std::make_shared<arrow::Buffer>(data, size);
+  auto buffer_reader = std::make_shared<arrow::io::BufferReader>(buffer);
+  auto options = arrow::ipc::IpcReadOptions::Defaults();
+  ARROW_ASSIGN_OR_RAISE(
+      auto reader, arrow::ipc::RecordBatchStreamReader::Open(buffer_reader, options));
+  ARROW_ASSIGN_OR_RAISE(auto batches, reader->ToRecordBatches());
+  return batches[0];
 }
 
 arrow::compute::Expression GetFilter(std::string selectivity) {
@@ -49,63 +68,63 @@ arrow::compute::Expression GetFilter(std::string selectivity) {
 }
 
 arrow::Result<std::shared_ptr<arrow::RecordBatchReader>> ScanDataset(cp::ExecContext& exec_context, std::string backend, std::string selectivity) {
-    std::string uri = "file:///mnt/cephfs/dataset";
+  std::string uri = "file:///mnt/cephfs/dataset";
 
-    auto schema = arrow::schema({
-      arrow::field("VendorID", arrow::int64()),
-      arrow::field("tpep_pickup_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
-      arrow::field("tpep_dropoff_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
-      arrow::field("passenger_count", arrow::int64()),
-      arrow::field("trip_distance", arrow::float64()),
-      arrow::field("RatecodeID", arrow::int64()),
-      arrow::field("store_and_fwd_flag", arrow::utf8()),
-      arrow::field("PULocationID", arrow::int64()),
-      arrow::field("DOLocationID", arrow::int64()),
-      arrow::field("payment_type", arrow::int64()),
-      arrow::field("fare_amount", arrow::float64()),
-      arrow::field("extra", arrow::float64()),
-      arrow::field("mta_tax", arrow::float64()),
-      arrow::field("tip_amount", arrow::float64()),
-      arrow::field("tolls_amount", arrow::float64()),
-      arrow::field("improvement_surcharge", arrow::float64()),
-      arrow::field("total_amount", arrow::float64())
-    });
+  auto schema = arrow::schema({
+    arrow::field("VendorID", arrow::int64()),
+    arrow::field("tpep_pickup_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+    arrow::field("tpep_dropoff_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+    arrow::field("passenger_count", arrow::int64()),
+    arrow::field("trip_distance", arrow::float64()),
+    arrow::field("RatecodeID", arrow::int64()),
+    arrow::field("store_and_fwd_flag", arrow::utf8()),
+    arrow::field("PULocationID", arrow::int64()),
+    arrow::field("DOLocationID", arrow::int64()),
+    arrow::field("payment_type", arrow::int64()),
+    arrow::field("fare_amount", arrow::float64()),
+    arrow::field("extra", arrow::float64()),
+    arrow::field("mta_tax", arrow::float64()),
+    arrow::field("tip_amount", arrow::float64()),
+    arrow::field("tolls_amount", arrow::float64()),
+    arrow::field("improvement_surcharge", arrow::float64()),
+    arrow::field("total_amount", arrow::float64())
+  });
+  
+  std::string path;
+  ARROW_ASSIGN_OR_RAISE(auto fs, arrow::fs::FileSystemFromUri(uri, &path)); 
+  auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
     
-    std::string path;
-    ARROW_ASSIGN_OR_RAISE(auto fs, arrow::fs::FileSystemFromUri(uri, &path)); 
-    auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
-      
-    arrow::fs::FileSelector s;
-    s.base_dir = std::move(path);
-    s.recursive = true;
+  arrow::fs::FileSelector s;
+  s.base_dir = std::move(path);
+  s.recursive = true;
 
-    arrow::dataset::FileSystemFactoryOptions options;
-    ARROW_ASSIGN_OR_RAISE(auto factory, 
-      arrow::dataset::FileSystemDatasetFactory::Make(std::move(fs), s, std::move(format), options));
-    arrow::dataset::FinishOptions finish_options;
-    ARROW_ASSIGN_OR_RAISE(auto dataset,factory->Finish(finish_options));
+  arrow::dataset::FileSystemFactoryOptions options;
+  ARROW_ASSIGN_OR_RAISE(auto factory, 
+    arrow::dataset::FileSystemDatasetFactory::Make(std::move(fs), s, std::move(format), options));
+  arrow::dataset::FinishOptions finish_options;
+  ARROW_ASSIGN_OR_RAISE(auto dataset,factory->Finish(finish_options));
 
-    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<cp::ExecPlan> plan,
-                          cp::ExecPlan::Make(&exec_context));
+  ARROW_ASSIGN_OR_RAISE(std::shared_ptr<cp::ExecPlan> plan,
+                        cp::ExecPlan::Make(&exec_context));
 
-    ARROW_ASSIGN_OR_RAISE(auto scanner_builder, dataset->NewScan());
-    ARROW_RETURN_NOT_OK(scanner_builder->Filter(GetFilter(selectivity)));
-    ARROW_RETURN_NOT_OK(scanner_builder->Project(schema->field_names()));
+  ARROW_ASSIGN_OR_RAISE(auto scanner_builder, dataset->NewScan());
+  ARROW_RETURN_NOT_OK(scanner_builder->Filter(GetFilter(selectivity)));
+  ARROW_RETURN_NOT_OK(scanner_builder->Project(schema->field_names()));
 
-    ARROW_ASSIGN_OR_RAISE(auto scanner, scanner_builder->Finish());
+  ARROW_ASSIGN_OR_RAISE(auto scanner, scanner_builder->Finish());
 
-    std::shared_ptr<arrow::RecordBatchReader> reader; 
-    if (backend == "dataset") {
-      std::cout << "Using dataset backend: " << uri << std::endl;
-      ARROW_ASSIGN_OR_RAISE(reader, scanner->ToRecordBatchReader());
-    } else if (backend == "dataset+mem") {
-      std::cout << "Using dataset+mem backend: " << uri << std::endl;
-      ARROW_ASSIGN_OR_RAISE(auto table, scanner->ToTable())
-      auto im_ds = std::make_shared<arrow::dataset::InMemoryDataset>(table);
-      ARROW_ASSIGN_OR_RAISE(auto im_ds_scanner_builder, im_ds->NewScan());
-      ARROW_ASSIGN_OR_RAISE(auto im_ds_scanner, im_ds_scanner_builder->Finish());
-      ARROW_ASSIGN_OR_RAISE(reader, im_ds_scanner->ToRecordBatchReader());
-    }
+  std::shared_ptr<arrow::RecordBatchReader> reader; 
+  if (backend == "dataset") {
+    std::cout << "Using dataset backend: " << uri << std::endl;
+    ARROW_ASSIGN_OR_RAISE(reader, scanner->ToRecordBatchReader());
+  } else if (backend == "dataset+mem") {
+    std::cout << "Using dataset+mem backend: " << uri << std::endl;
+    ARROW_ASSIGN_OR_RAISE(auto table, scanner->ToTable())
+    auto im_ds = std::make_shared<arrow::dataset::InMemoryDataset>(table);
+    ARROW_ASSIGN_OR_RAISE(auto im_ds_scanner_builder, im_ds->NewScan());
+    ARROW_ASSIGN_OR_RAISE(auto im_ds_scanner, im_ds_scanner_builder->Finish());
+    ARROW_ASSIGN_OR_RAISE(reader, im_ds_scanner->ToRecordBatchReader());
+  }
 
-    return reader;
+  return reader;
 }
